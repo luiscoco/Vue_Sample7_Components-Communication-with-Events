9820ed7e868a99300d94b6a8494d14f336ba0f2a
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-06-13 09:23:24 +0200

    first-commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,15 @@
+<!-- ChildComponent.vue -->
+<template>
+  <button @click="emitCustomEvent">Emit Custom Event</button>
+</template>
+
+<script lang="ts" setup>
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["custom-event"]);
+
+const emitCustomEvent = () => {
+  const payload = "Hello from ChildComponent!";
+  emit("custom-event", payload);
+};
+</script>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,21 @@
+<!-- ParentComponent.vue -->
+<template>
+  <div>
+    <ChildComponent @custom-event="handleCustomEvent" />
+    <p>{{ payloadMessage }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineEmits, ref } from "vue";
+import ChildComponent from "./ChildComponent.vue";
+
+defineEmits(["custom-event"]);
+
+const payloadMessage = ref(""); // Create a reactive variable to hold the payload message
+
+const handleCustomEvent = (payload: unknown) => {
+  console.log("Received custom event:", payload);
+  payloadMessage.value = String(payload); // Update the payload message variable
+};
+</script>
